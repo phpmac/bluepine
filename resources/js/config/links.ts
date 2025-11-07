c774472af7ad500832fa645f5811c1202cb65a58
a <a@vc.sb>
2025-11-07 13:07:09 +0800

    自动下载

--- resources/js/config/links.ts
+++ resources/js/config/links.ts
@@ -1,12 +1,12 @@
 // 白皮书链接 (根据语言返回不同文件)
 export const getWhitepaperUrl = (isEnglish: boolean) => {
     return isEnglish
-        ? 'https://github.com/bluepinefoundation/token/blob/main/AESC%20Whitepaper.pdf'
-        : 'https://github.com/bluepinefoundation/token/blob/main/AESC%E7%99%BD%E7%9A%AE%E4%B9%A6%EF%BC%88%E4%B8%AD%E6%96%87%E7%89%88%EF%BC%89.pdf';
+        ? 'https://github.com/bluepinefoundation/token/raw/refs/heads/main/AESC%20Whitepaper.pdf'
+        : 'https://github.com/bluepinefoundation/token/raw/refs/heads/main/AESC%E7%99%BD%E7%9A%AE%E4%B9%A6%EF%BC%88%E4%B8%AD%E6%96%87%E7%89%88%EF%BC%89.pdf';
 };
 
 // 审计报告链接
-export const auditReportUrl = 'https://github.com/bluepinefoundation/token/blob/main/REP-AESC__final-20251020T011921Z.pdf';
+export const auditReportUrl = 'https://github.com/bluepinefoundation/token/raw/refs/heads/main/REP-AESC__final-20251020T011921Z.pdf';
 
 // 品牌资产链接 (根据语言返回不同文件)
 export const getBrandAssetsUrl = (isEnglish: boolean) => {
