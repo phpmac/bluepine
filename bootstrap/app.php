6f316f416a8693fe90527aa2c98669f062dbda33
a <a@vc.sb>
2025-11-09 01:38:17 +0800

    csrf和apip信息配置

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,14 +11,16 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        channels: __DIR__.'/../routes/channels.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
-        ]);
+        ])
+            ->validateCsrfTokens(except: [
+                'notify',
+            ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
